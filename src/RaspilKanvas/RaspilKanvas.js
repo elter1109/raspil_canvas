792e273e414afe345bcf80a1d38a3ae39c7b5d6f
Elena Tereshina <vpelena110981@gmail.com>
2020-07-20 15:33:04 +0300

    1

--- src/RaspilKanvas/RaspilKanvas.js
+++ src/RaspilKanvas/RaspilKanvas.js
@@ -8,147 +8,163 @@ import Spinner from '../Spinner/Spinner';
 import classes from './RaspilKanvas.module.scss';
 
 const METRIC_SIZE = 20;
+const DISTANCE = 33;
 
-// function Kromka({
-//   x,
-//   y,
-//   newWidth,
-//   newLength,
-//   straightKromka,
-//   decorKromka1mm,
-//   decorKromka2mm,
-//   decorKromka04mm,
-// }) {
-//   const {top, bottom, left, right} = straightKromka;
-//   const [image] = useImage(path);
-//   const distance = 33;
- 
+function Kromka({
+  x,
+  y,
+  newWidth,
+  newLength,
+  straightKromka,
+  srcKromka1mm,
+  srcKromka2mm,
+  srcKromka04mm,
+}) {
+  const { top, bottom, left, right } = straightKromka;
 
-//   let top, topText, bottom, bottomText, right, rightText, left, leftText;
-//   if (thickTop) {
-//     top = 7;
-//     topText = '2.0';
-//   } else if (thinkTop) {
-//     top = 3;
-//     topText = '0.4';
-//   } else {
-//     top = '';
-//     topText = '0.0';
-//   }
-//   if (thickBottom) {
-//     bottom = 7;
-//     bottomText = '2.0';
-//   } else if (thinkBottom) {
-//     bottom = 3;
-//     bottomText = '0.4';
-//   } else {
-//     bottom = '';
-//     bottomText = '0.0';
-//   }
-//   if (thickRight) {
-//     right = 7;
-//     rightText = '2.0';
-//   } else if (thinkRight) {
-//     right = 3;
-//     rightText = '0.4';
-//   } else {
-//     right = '';
-//     rightText = '0.0';
-//   }
-//   if (thickLeft) {
-//     left = 7;
-//     leftText = '2.0';
-//   } else if (thinkLeft) {
-//     left = 3;
-//     leftText = '0.4';
-//   } else {
-//     left = '';
-//     leftText = '0.0';
-//   }
-//   return (
-//     <React.Fragment>
-//       <Group x={x} y={y - distance}>
-//         <Image
-//           image={image}
-//           height={top}
-//           x={0}
-//           y={0}
-//           width={newWidth}
-//           shadowBlur={10}
-//           shadowOffset={{ x: 10, y: 10 }}
-//           shadowOpacity={0.3}
-//         />
-//         <Text
-//           text={topText}
-//           padding={10}
-//           x={newWidth / 2 - 10}
-//           y={0}
-//           fontStyle='bold'
-//         />
-//       </Group>
+  const renderComponent = Object.entries(straightKromka).map((el) => {
+    console.log(el);
+    let render;
+    if (el !== '') {
+      render = (
+        <Group x={x} y={y - DISTANCE}>
+          <TextureImage
+            path={`srcKromka${el}`}
+            newWidth={10}
+            newLength={10}
+            x={x}
+            y={y}
+          />
+          {/* <Image
+            image={image}
+            height={top}
+            x={0}
+            y={0}
+            width={newWidth}
+            shadowBlur={10}
+            shadowOffset={{ x: 10, y: 10 }}
+            shadowOpacity={0.3}
+          />
+          <Text
+            text={topText}
+            padding={10}
+            x={newWidth / 2 - 10}
+            y={0}
+            fontStyle='bold'
+          /> */}
+        </Group>
+      );
+    } else {
+      render = null;
+    }
+  });
 
-//       <Group x={x} y={y + newHeight + distance}>
-//         <Image
-//           image={image}
-//           height={bottom}
-//           x={0}
-//           y={0}
-//           width={newWidth}
-//           shadowBlur={10}
-//           shadowOffset={{ x: 10, y: 10 }}
-//           shadowOpacity={0.3}
-//         />
-//         <Text
-//           text={bottomText}
-//           padding={10}
-//           x={newWidth / 2 - 10}
-//           y={0}
-//           fontStyle='bold'
-//         />
-//       </Group>
+  // let top, topText, bottom, bottomText, right, rightText, left, leftText;
+  // if (thickTop) {
+  //   top = 7;
+  //   topText = '2.0';
+  // } else if (thinkTop) {
+  //   top = 3;
+  //   topText = '0.4';
+  // } else {
+  //   top = '';
+  //   topText = '0.0';
+  // }
+  // if (thickBottom) {
+  //   bottom = 7;
+  //   bottomText = '2.0';
+  // } else if (thinkBottom) {
+  //   bottom = 3;
+  //   bottomText = '0.4';
+  // } else {
+  //   bottom = '';
+  //   bottomText = '0.0';
+  // }
+  // if (thickRight) {
+  //   right = 7;
+  //   rightText = '2.0';
+  // } else if (thinkRight) {
+  //   right = 3;
+  //   rightText = '0.4';
+  // } else {
+  //   right = '';
+  //   rightText = '0.0';
+  // }
+  // if (thickLeft) {
+  //   left = 7;
+  //   leftText = '2.0';
+  // } else if (thinkLeft) {
+  //   left = 3;
+  //   leftText = '0.4';
+  // } else {
+  //   left = '';
+  //   leftText = '0.0';
+  // }
+  return (
+    <React.Fragment>
+      {renderComponent}
+      {/* <Group x={x} y={y + newHeight + distance}>
+        <Image
+          image={image}
+          height={bottom}
+          x={0}
+          y={0}
+          width={newWidth}
+          shadowBlur={10}
+          shadowOffset={{ x: 10, y: 10 }}
+          shadowOpacity={0.3}
+        />
+        <Text
+          text={bottomText}
+          padding={10}
+          x={newWidth / 2 - 10}
+          y={0}
+          fontStyle='bold'
+        />
+      </Group>
 
-//       <Group x={x + newWidth + distance} y={y}>
-//         <Image
-//           image={image}
-//           height={newHeight}
-//           x={0}
-//           y={0}
-//           width={right}
-//           shadowBlur={10}
-//           shadowOffset={{ x: 10, y: 10 }}
-//           shadowOpacity={0.3}
-//         />
-//         <Text
-//           text={rightText}
-//           padding={10}
-//           x={0}
-//           y={newHeight / 2 - 10}
-//           fontStyle='bold'
-//         />
-//       </Group>
+      <Group x={x + newWidth + distance} y={y}>
+        <Image
+          image={image}
+          height={newHeight}
+          x={0}
+          y={0}
+          width={right}
+          shadowBlur={10}
+          shadowOffset={{ x: 10, y: 10 }}
+          shadowOpacity={0.3}
+        />
+        <Text
+          text={rightText}
+          padding={10}
+          x={0}
+          y={newHeight / 2 - 10}
+          fontStyle='bold'
+        />
+      </Group>
 
-//       <Group x={x - distance} y={y}>
-//         <Image
-//           image={image}
-//           height={newHeight}
-//           x={0}
-//           y={0}
-//           width={left}
-//           shadowBlur={10}
-//           shadowOffset={{ x: 10, y: 10 }}
-//           shadowOpacity={0.3}
-//         />
-//         <Text
-//           text={leftText}
-//           padding={10}
-//           x={0}
-//           y={newHeight / 2 - 10}
-//           fontStyle='bold'
-//         />
-//       </Group>
-//     </React.Fragment>
-//   );
-// }
+      <Group x={x - distance} y={y}>
+        <Image
+          image={image}
+          height={newHeight}
+          x={0}
+          y={0}
+          width={left}
+          shadowBlur={10}
+          shadowOffset={{ x: 10, y: 10 }}
+          shadowOpacity={0.3}
+        />
+        <Text
+          text={leftText}
+          padding={10}
+          x={0}
+          y={newHeight / 2 - 10}
+          fontStyle='bold'
+        />
+      </Group> */}
+    </React.Fragment>
+  );
+}
 
 function Metric({ size, newLength, newWidth, x, y, vertical }) {
   let xGroup,
@@ -203,7 +219,7 @@ function Metric({ size, newLength, newWidth, x, y, vertical }) {
   );
 }
 
-function TextureImage ({ path, newWidth, newLength, x, y }) {
+function TextureImage({ path, newWidth, newLength, x, y }) {
   const myRef = useRef(null);
   const [image, status] = useImage(path);
   useEffect(() => {
@@ -226,13 +242,10 @@ function TextureImage ({ path, newWidth, newLength, x, y }) {
       shadowOpacity={0.5}
     />
   );
-};
+}
 
 export default function RaspilKanvas({ data, spravka }) {
-  const {
-    length,
-    width,
-  } = data;
+  const { length, width, plate: {type: typePlate, value: valuePlate} } = data;
 
   function calcProportion(value1, value2) {
     const newMaxValue = Math.max(value1, value2);
@@ -259,12 +272,26 @@ export default function RaspilKanvas({ data, spravka }) {
   const x = Math.round((STAGE_SIZE - newLength) / 2);
   const y = Math.round((STAGE_SIZE - newWidth) / 2);
 
+  const srcKromka2mm =
+    data.kromka2mm && data.kromka2mm.value !== 'no_kromka'
+      ? spravka.decors[data.kromka2mm.value].src
+      : undefined;
+  const srcKromka04mm =
+    data.kromka04mm && data.kromka04mm.value !== 'no_kromka'
+      ? spravka.decors[data.kromka04mm.value].src
+      : undefined;
+  const srcKromka1mm =
+    data.kromka1mm && data.kromka1mm.value !== 'no_kromka'
+      ? spravka.decors[data.kromka1mm.value].src
+      : undefined;
+     
+
   return (
     <div className={classes.CanvFigures}>
       <Stage height={STAGE_SIZE} width={STAGE_SIZE}>
         <Layer>
           <TextureImage
-            path={spravka.decors[data.plate.value].src}
+            path={spravka.decors[valuePlate].src}
             newLength={newLength}
             newWidth={newWidth}
             x={x}
@@ -285,16 +312,18 @@ export default function RaspilKanvas({ data, spravka }) {
             x={x}
             y={y}
           />
-          {/* <Kromka
-            x={x}
-            y={y}
-            decorKromka2mm={spravka.decors[data.kromka2mm.value].src}
-            decorKromka04mm={spravka.decors[data.kromka04mm.value].src}
-            decorKromka1mm={spravka.decors[data.kromka1mm.value].src}
-            newWidth={newWidth}
-            newLength={newLength}
-            straightKromka={data.straightKromka}
-          /> */}
+          {typePlate !== 'orgalit' ? (
+            <Kromka
+              x={x}
+              y={y}
+              srcKromka2mm={srcKromka2mm}
+              srcKromka04mm={srcKromka04mm}
+              srcKromka1mm={srcKromka1mm}
+              newWidth={newWidth}
+              newLength={newLength}
+              straightKromka={data.straightKromka ?? undefined}
+            />
+          ) : null}
         </Layer>
       </Stage>
     </div>
